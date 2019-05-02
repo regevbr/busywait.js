6660e6ed2ee33bc37bd65c9f87568d0f7b173419
regevbr <regevbr@gmail.com>
2019-05-02 14:34:43 +0300

    #9 add typescript support

--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -x
-set -e
-./node_modules/.bin/ncu -u --semverLevel newest --upgradeAll -e 2 || (./node_modules/.bin/ncu -u --semverLevel newest --upgradeAll -e 1 && npm install)
